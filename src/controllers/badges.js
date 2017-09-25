ecad3bc8ab9521633d622fe46e8e88d3ad0f0b2e
shekit <as7790@nyu.edu>
2017-09-25 00:46:29 -0400

    implemented routing structure, added mock controller for badges

---
+++ src/controllers/badges.js
@@ -0,0 +1,11 @@
+export const list = (req, res) => {
+  //res.send("get badge list");
+  console.log("get badge list");
+  res.send("got badge list");
+};
+
+export const create = (req, res) => {
+  //res.send("create a badge");
+  console.log("create badge");
+  res.send("badge created");
+};
