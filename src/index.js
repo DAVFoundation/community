b87e6a2e16662013d141350475ba81a3f20588e8
shekit <as7790@nyu.edu>
2017-09-26 14:34:25 -0400

    included mongoose and mongodb link

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import app from './app';
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mongo:27017')
+  .then(() => console.log('connected to DB'))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("api server started on port 3000");
