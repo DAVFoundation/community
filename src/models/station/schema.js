8678952d7e6edef3b5d023d873666714250c73f4
shekit <as7790@nyu.edu>
2017-09-27 03:41:59 -0400

    added station schema

---
+++ src/models/station/schema.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import timestamp from 'mongoose-timestamp';
+
+const Schema = mongoose.Schema;
+
+const stationSchema = new Schema({
+  owner:{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+stationSchema.plugin(timestamp);
+
+export default stationSchema;
