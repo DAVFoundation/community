c668a6f65ede492060fd96f38f4fe894461f3d47
shekit <as7790@nyu.edu>
2017-09-28 03:58:11 -0400

    added async award badge function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,16 +1,29 @@
 import randomstring from 'randomstring';
+import DavAccount from '../models/davAccount/model';
+import Update from '../models/update/model';
+import Badge from '../models/badge/model';
+import User from '../models/user/model';
 
 export function randomDavAddress(){
+  console.log("Random DAV address generated");
   return '0x'+randomstring.generate({
     length: 40,
     charset: 'hex'
   });
 }
 
-export function awardBadge(user, badgeSlug){
+export const awardBadge = async (user, badgeSlug) => {
   console.log(`${user.name} is awarded ${badgeSlug} badge`);
-}
 
-export function createUpdate(user, update){
-  console.log(`${user.name} has a new update: ${update.description}`);
-}
+  let badge = await Badge.findOne({slug:badgeSlug}).exec();
+
+  let owner = await User.findOneAndUpdate({id:user._id}, {$push:{badges:badge._id}}).exec();
+
+  createUpdate(owner,{
+    description: `${owner.name} was awarded the ${badge.title} badge`
+  });
+  // add badge to user
+  // add update about badge add to user
+};
+
+
