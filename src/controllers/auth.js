f9d27a617a3b1f62edd42d28511fa4f6e7b2489d
shekit <as7790@nyu.edu>
2017-09-28 05:38:27 -0400

    implemented async await signup flow

---
+++ src/controllers/auth.js
@@ -0,0 +1,46 @@
+import User from '../models/user/model';
+import {awardBadge, createUpdate, createDavAccount, followUser} from '../lib/utils';
+
+export const login = (req, res) => {
+  res.send("logged in");
+};
+
+export const signup = (req, res) => {
+
+  saveUser(res);
+
+};
+
+const saveUser = async (res) => {
+  let user = {
+    name: "Ann Arbor",
+    email: "a@7.com",
+    password: "test"
+  };
+
+  let newuser = await User.create(user);
+  console.log("user created");
+
+  await createDavAccount(newuser);
+  console.log("dav account created");
+
+  await createUpdate(newuser,{
+    description: `${user.name} has joined DAV`
+  });
+  console.log("update created");
+
+  let cutoff = new Date("2017-12-31");
+  if(newuser.createdAt<=cutoff){
+    console.log("he is a founder");
+    await awardBadge(newuser,"founder");
+    console.log("founder badge added");
+  }
+
+  let updatedUser = await followUser(newuser, {uid:"0xf2a0b28a62aebd3b7e17592b920cf7ec10470d02"});
+  //let updatedUser = await User.findById(newuser._id).exec();
+  console.log("started following main account");
+  res.json(updatedUser);
+
+};
+
+//"0xa725aa4a54328940f503d082194d7aca67ac4341"
