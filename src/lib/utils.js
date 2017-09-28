599de37d67b63c7175aec1bb94b902848cfa291e
shekit <as7790@nyu.edu>
2017-09-28 00:57:51 -0400

    added placeholder functions for adding badge and update

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,16 @@
+import randomstring from 'randomstring';
+
 export function randomDavAddress(){
-  return '0x'+randomString.generate({
+  return '0x'+randomstring.generate({
     length: 40,
     charset: 'hex'
   });
-};
+}
+
+export function awardBadge(user, badgeSlug){
+  console.log(`${user.name} is awarded ${badgeSlug} badge`);
+}
+
+export function createUpdate(user, update){
+  console.log(`${user.name} has a new update: ${update.description}`);
+}
