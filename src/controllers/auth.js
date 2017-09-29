dbd21582169844c1e95d5e3278a696f4fbbb9f61
shekit <as7790@nyu.edu>
2017-09-29 17:49:53 -0400

    refactored signup code, cleaned up imports

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,7 @@
-import User from '../models/user/model';
-import {awardBadge, createUpdate, createDavAccount,createUser, followUser, savePerson} from '../lib/utils';
+import config from '../config';
+import Person from '../models/person/model';
+import {awardBadge, createUpdate, createThing, followPerson} from '../lib/utils';
+
 
 export const login = (req, res) => {
   res.send("logged in");
@@ -7,19 +9,36 @@ export const login = (req, res) => {
 
 export const signup = async (req, res, next) => {
 
-  let existingPerson = await User.findOne({email: req.body.email}).exec();
+  let existingPerson = await Person.findOne({email: req.body.email}).exec();
 
   if(existingPerson){
     console.log("already exists");
-    return res.send("user already exists");
+    return res.send("person already exists");
   }
 
-  console.log("new user signing up");
+  console.log("new person signing up");
 
-  let newPerson = await savePerson(req.body);
+  let person = await createThing({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }, config.accountType.person);
+
+  await createUpdate(person, {
+    description: `${person.name} has joined DAV`
+  });
+
+  if(person.createdAt <= config.cutoffDate){
+    await awardBadge(person, "founder");
+  }
+
+  let mainDav = await Person.findOne({email: config.dav.email}).exec();
+
+  let updatedPerson = await followPerson(person, {uid:mainDav.uid});
+
   console.log("person is fully created");
   console.log("sending response now");
-  res.json(newPerson);
+  res.json(updatedPerson);
 
 };
 
