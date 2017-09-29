7020cab1cce106b509ba14709b89a23a28d20a9f
shekit <as7790@nyu.edu>
2017-09-29 17:47:51 -0400

    corrected update error

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -41,8 +41,8 @@ export const awardBadge = async (person, badgeSlug) => {
 
   let badge = await Badge.findOne({slug:badgeSlug}).exec();
 
-  await createUpdate(awardee,{
-    description: `${owner.name} was awarded the ${badge.title} badge`
+  await createUpdate(person,{
+    description: `${person.name} was awarded the ${badge.title} badge`
   });
 
   return Person.findByIdAndUpdate(person._id, {$push:{badges:badge._id}}, {new:true}).exec();
@@ -71,33 +71,4 @@ export const followPerson = async (person, followee) => {
 
 };
 
-export const createMainDavAccount = async () => {
 
-  let person = {
-    name:config.dav.name,
-    email:config.dav.email,
-    password:config.dav.password
-  };
-
-  let existingPerson = await Person.findOne({email: person.email}).exec();
-
-  if(existingPerson){
-    console.log("Main account already exists");
-    return;
-  }
-
-  console.log("Creating main account");
-  let mainAccount = await createThing(person, config.accountType.person);
-  console.log("Created main account");
-
-  await createUpdate(mainAccount, {
-    description: `${mainAccount.name} has joined DAV`
-  });
-
-  if(person.createdAt <= config.cutoffDate){
-    await awardBadge(mainAccount, "founder");
-  }
-
-  console.log("Returning new main account");
-  return mainAccount;
-};
