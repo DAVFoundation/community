ce8e4c338ba155995968f286bd85c229993fd349
shekit <as7790@nyu.edu>
2017-09-29 18:26:10 -0400

    implemented local strategy to login user

--- src/passport/local.js
+++ src/passport/local.js
@@ -1,11 +1,27 @@
 import passportLocal from 'passport-local';
 import bcrypt from 'bcrypt-nodejs';
-import User from '../models/user/model';
+import Person from '../models/person/model';
 
 const LocalStrategy = passportLocal.Strategy;
 
 export default login = (passport) => {
 
   passport.use('local-login', new LocalStrategy({
-  }))
+    usernameField : 'email',
+    passReqToCallback: true
+  },
+  async (req, email, password, done) => {
+
+    let existingUser = await Person.findOne({email: email}).exec();
+
+    if(!existingUser) return done(null, false);
+
+    let correctPassword = await user.comparePassword(password);
+
+    if(!correctPassword) return done(null, false);
+
+    return done(null, user);
+
+  });
+  );
 };
