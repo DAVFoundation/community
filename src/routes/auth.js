bdd7ac80f667555d17dc91946e22f76ccadd12b7
shekit <as7790@nyu.edu>
2017-09-30 04:44:49 -0400

    added login response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,13 +7,17 @@ export default function(passport){
 
   router.route('/login')
     .post((req, res, next) => {
+
       passport.authenticate('local-login', (err, user, info) => {
         if(err) return next(err);
-
         if(!user) return res.send("no user exists");
-
+        console.log("found user");
+        console.log("user");
         req.login(user, function(err){
-          return res.json(user);
+          //return res.json(user);
+          console.log("login user");
+          //res.cookie("user", user._id);
+          res.json(user);
         });
 
       })(req, res, next);
