a38c84f48ac91b58569307dda405bbc0270c8718
shekit <as7790@nyu.edu>
2017-09-30 15:21:07 -0400

    return user if authenticated

--- src/controllers/person.js
+++ src/controllers/person.js
@@ -2,8 +2,16 @@ import Person from '../models/person/model';
 
 export const single = async (req, res) => {
 
-  let person = await Person.findById(req.params.userId).exec();
+  if(req.isAuthenticated()){
+    console.log("Authenticated");
+    return res.json(req.user);
+  }
 
-  res.json(person);
+  return res.status(403).send("Access Denied");
 
-}
+
+  // let person = await Person.findById(req.params.userId).exec();
+
+  // res.json(person);
+
+};
