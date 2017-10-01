0b90d1f837c6cd449224bbcd10d5ec5a76b86c72
shekit <as7790@nyu.edu>
2017-09-30 22:21:52 -0400

    added user badges route, fixed error with query

--- src/controllers/person.js
+++ src/controllers/person.js
@@ -1,4 +1,6 @@
 import Person from '../models/person/model';
+import Badge from '../models/badge/model';
+import mongoose from 'mongoose';
 
 export const single = async (req, res) => {
 
@@ -14,4 +16,39 @@ export const single = async (req, res) => {
 
   // res.json(person);
 
+};
+
+export const badges = async (req, res) => {
+
+  if(req.isAuthenticated()){
+
+    let badgeIds = req.user.badges.map((b) => {
+      return b.badge;
+    });
+
+    let bs = await Badge.find({_id:{$in:badgeIds}}).exec();
+
+    let badgesById = {};
+
+    bs.map((badge) => {
+      badgesById[badge._id] = badge;
+    });
+
+    console.log(badgesById);
+
+    return res.json({
+      badgeIds:badgeIds,
+      badgesById:badgesById
+    });
+  }
+
+  return res.status(403).send("Access Denied");
+};
+
+export const updates = async (req, res) => {
+
+  if(req.isAuthenticated()){
+    console.log("yes");
+  }
+  return res.status(403).send("Access Denied");
 };
