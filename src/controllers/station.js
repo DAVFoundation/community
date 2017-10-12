798bcef8bdbc60480509d05f22b54cc233c89054
shekit <as7790@nyu.edu>
2017-10-12 15:25:44 -0400

    added station create controller

--- src/controllers/station.js
+++ src/controllers/station.js
@@ -1,26 +1,32 @@
 import config from '../config';
 import Station from '../models/station/model';
-import {awardBadge, createUpdate, createThing, followPerson} from '../lib/utils';
+import {awardBadge, createUpdate, createThing} from '../lib/utils';
+
+export const test = async(req, res, next) => {
+  return res.json({"what":"tf"});
+};
 
 export const create = async (req, res) => {
-
+  console.log("CREATE STATIon");
   if(!req.isAuthenticated()){
     return res.status(403).send("Access Denied");
   }
 
-  let stationDetails = req.body.formData;
+  let stationDetails = Object.assign({},req.body, {
+    person: req.user._id
+  });
 
-  stationDetails.person = req.user._id;
+  console.log(stationDetails);
 
-  let station = await createThing(status, config.accountType.station);
+  let station = await createThing(stationDetails, config.accountType.station);
   console.log("created base station");
   console.log(station);
   await createUpdate(req.user, {
-    description: `${req.user.name} added a ${req.body.formData.type} station`
+    description: `${req.user.name} added a ${req.body.type} station`
   });
   console.log("created station update");
-  await awardBadge(req.user, "station-owner");
+  await awardBadge(req.user, "station");
   console.log("station is fully created");
 
   res.json({"success":true});
-}
+};
