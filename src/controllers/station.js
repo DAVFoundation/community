a53dacc7c821aa88a0dad30b6428b70f06e34ce0
shekit <as7790@nyu.edu>
2017-10-16 19:45:58 -0400

    combined queries into single one

--- src/controllers/station.js
+++ src/controllers/station.js
@@ -27,32 +27,11 @@ export const create = async (req, res) => {
     description: `${req.user.name} added a ${req.body.type} station`
   });
 
-  let user = await Person.findById(req.user._id).populate('stations').exec();
-
-  let stationType = req.body.type;
-  console.log("STATION TYPE", stationType);
-  console.log("before station save");
-
   let key = req.body.type;
   let dynSet = {$set: {}};
   dynSet.$set["hasStation."+key] = true;
 
-
-  let newuser = await Person.findByIdAndUpdate(req.user._id, dynSet, {new: true}).exec();
-
-  // var newuser = null;
-
-
-  // await Person.findById(req.user._id, function(err, doc){
-  //   doc.hasStation[stationType] = true;
-  //   doc.save(function(err, newdoc){
-  //     console.log("NEW DOC");
-  //     console.log(newdoc);
-  //     newuser = newdoc;
-  //   });
-  // });
-  console.log("after station save");
-  console.log(newuser);
+  let user = await Person.findByIdAndUpdate(req.user._id, dynSet, {new: true}).populate('stations').exec();
 
   if(user.stations.length == 1){
     await awardBadge(req.user, "station-master");
