25cc61cd5d026b87a8d3ff71d482af3418fb4564
shekit <as7790@nyu.edu>
2017-10-17 12:08:39 -0400

    only add points if under certain threshold, uncommented all bounding boxes

--- src/lib/dummyData.js
+++ src/lib/dummyData.js
@@ -9,74 +9,74 @@ export const generateRandomStations = async () => {
   const boundingBoxes = [
     {
       country: "usa",
-      count: 249,
+      count: 2439,
       bbox: [-122.3,31.1,-72.1,49.6],
     },
-    // {
-    //   country: "mexico",
-    //   count: 706,
-    //   bbox: [-109.5,16.1,-94.0,33.3],
-    // },
-    // {
-    //   country: "canada",
-    //   count: 1508,
-    //   bbox: [-128.3,50.7,-93.2,67.6],
-    // },
-    // {
-    //   country: "southamerica",
-    //   count: 1924,
-    //   bbox: [-76.8,-19.6,-39.2,0.4],
-    // },
-    // {
-    //   country: "southamericalower",
-    //   count: 893,
-    //   bbox: [-69.7,-37.7,-53.4,-20.3],
-    // },
-    // {
-    //   country: "europe",
-    //   count: 2432,
-    //   bbox: [1.1,43.1,29.2,53.1],
-    // },
-    // {
-    //   country: "europeupper",
-    //   count: 984,
-    //   bbox: [5.0,57.9,30.9,70.6],
-    // },
-    // {
-    //   country: "africa",
-    //   count: 1303,
-    //   bbox: [-14.8,7.4,35.5,34.0],
-    // },
-    // {
-    //   country: "africalower",
-    //   count: 635,
-    //   bbox: [13.9,-22.6,36.2,5.6],
-    // },
-    // {
-    //   country: "asia",
-    //   count: 3928,
-    //   bbox: [56.3,35.5,137.1,71.2],
-    // },
-    // {
-    //   country: "india",
-    //   count: 730,
-    //   bbox: [71.4,12.9,87.2,34.0],
-    // },
-    // {
-    //   country: "australia",
-    //   count: 734,
-    //   bbox: [114.8,-35.7,150.8,-14.9],
-    // },
-    // {
-    //   country: "japan",
-    //   count: 347,
-    //   bbox: [135.59,33.62,142.85,40.07],
-    // },
-    // {
-    //   country: "uk",
-    //   count: 306,
-    //   bbox: [-6.13,50.45,1.46,57.29]
-    // }
+    {
+      country: "mexico",
+      count: 706,
+      bbox: [-109.5,16.1,-94.0,33.3],
+    },
+    {
+      country: "canada",
+      count: 1508,
+      bbox: [-128.3,50.7,-93.2,67.6],
+    },
+    {
+      country: "southamerica",
+      count: 1924,
+      bbox: [-76.8,-19.6,-39.2,0.4],
+    },
+    {
+      country: "southamericalower",
+      count: 893,
+      bbox: [-69.7,-37.7,-53.4,-20.3],
+    },
+    {
+      country: "europe",
+      count: 2432,
+      bbox: [1.1,43.1,29.2,53.1],
+    },
+    {
+      country: "europeupper",
+      count: 984,
+      bbox: [5.0,57.9,30.9,70.6],
+    },
+    {
+      country: "africa",
+      count: 1303,
+      bbox: [-14.8,7.4,35.5,34.0],
+    },
+    {
+      country: "africalower",
+      count: 635,
+      bbox: [13.9,-22.6,36.2,5.6],
+    },
+    {
+      country: "asia",
+      count: 3928,
+      bbox: [56.3,35.5,137.1,71.2],
+    },
+    {
+      country: "india",
+      count: 730,
+      bbox: [71.4,12.9,87.2,34.0],
+    },
+    {
+      country: "australia",
+      count: 734,
+      bbox: [114.8,-35.7,150.8,-14.9],
+    },
+    {
+      country: "japan",
+      count: 347,
+      bbox: [135.59,33.62,142.85,40.07],
+    },
+    {
+      country: "uk",
+      count: 306,
+      bbox: [-6.13,50.45,1.46,57.29]
+    }
   ];
 
   let points = await DummyData.find({}).exec();
