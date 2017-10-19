db53d76e16a2247eeee644e171d5405f2d726a81
Tal Ater <tal@talater.com>
2017-10-19 19:29:30 +0300

    webpack.prod.js is now a copy of the dev copy
    Temporary because old prod was broken

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,20 @@ const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
 
 module.exports = merge(common, {
+  devtool: 'inline-source-map',
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV':JSON.stringify('production')
-      }
+    new webpack.NamedModulesPlugin(),
+
+    new webpack.BannerPlugin({
+      banner:'require("source-map-support").install();',
+      raw:true,
+      entryOnly: false
+    }),
+
+    new webpack.BannerPlugin({
+      banner:'require("babel-polyfill");',
+      raw:true,
+      entryOnly:false
     })
   ]
-})
+});
