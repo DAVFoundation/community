80a3bd7590eb1be946ea032530c2724413aff14c
Tal Ater <tal@talater.com>
2017-10-22 23:23:19 +0300

    Disable drop db migration

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,10 +1,10 @@
-import dropDb from './1-drop-db';
+// import dropDb from './1-drop-db';
 import createInitialBadges from './2-create-initial-badges';
 import createDavAccount from './3-create-dav-account';
 import seedRandomStations from './4-seed-random-stations';
 
 export default [
-  dropDb,
+  // dropDb,
   createInitialBadges,
   createDavAccount,
   seedRandomStations,
