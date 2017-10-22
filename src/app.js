436a57e6a5bb34124c0b4edd3e4c454e9db6bcd3
shekit <as7790@nyu.edu>
2017-10-22 14:43:32 -0400

    included mongo store for session persistence

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ import uuid from 'uuid';
 import config from './config';
 import passportConfig from './passport/init';
 import path from 'path';
+import connectMongo from 'connect-mongo';
+
+const MongoStore = connectMongo(session);
 
 const app = express();
 
@@ -25,14 +28,22 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const store = new MongoStore({url:config.mongo.url});
+
 app.use(session({
   genid: (req) => {
     return uuid.v4();
   },
   secret: config.session.secret,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: store
 }));
+
+store.on('connected', (e) => {
+  console.log("mongo store connected");
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 passportConfig(passport);
