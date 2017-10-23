bb0230642ab4dd3513c7ba445a086c9fb97ec106
Tal Ater <tal@talater.com>
2017-10-23 11:21:34 +0300

    Deleted drop db migration completely

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,10 +1,8 @@
-// import dropDb from './1-drop-db';
 import createInitialBadges from './2-create-initial-badges';
 import createDavAccount from './3-create-dav-account';
 import seedRandomStations from './4-seed-random-stations';
 
 export default [
-  // dropDb,
   createInitialBadges,
   createDavAccount,
   seedRandomStations,
