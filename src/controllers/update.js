d227029b83887e06fdc0bef383344f40ea1738bc
shekit <as7790@nyu.edu>
2017-11-09 23:58:44 -0500

    added route controller to add dav update

--- src/controllers/update.js
+++ src/controllers/update.js
@@ -1,13 +1,18 @@
 import Update from '../models/update/model';
+import DavAccount from '../models/davAccount/model';
+import Person from '../models/person/model';
+import config from '../config';
 
 export const create = async (req, res) => {
 
-  let account = await DavAccount.findById(req.user.account.id).exec();
+  let person = await Person.findOne({email:config.dav.email}).exec();
 
-  let updateDetails = Object.assign({}. req.body, {
-    davAccount: req.user.account.id,
-    name: req.user.name,
-    avatar: req.user.avatar
+  let account = await DavAccount.findById(person.account.id).exec();
+
+  let updateDetails = Object.assign({}, req.body, {
+    davAccount: person.account.id,
+    name: person.name,
+    avatar: person.avatar
   });
 
   let newUpdate = await Update.create(updateDetails);
