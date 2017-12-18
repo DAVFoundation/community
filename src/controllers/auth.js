b943ebcf8b60902e5bfa65b1be2455000888713d
shekit <as7790@nyu.edu>
2017-12-18 17:56:25 -0500

    adjusted password reset link

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -106,7 +106,7 @@ export const reset = async(req, res, next) => {
     subject: 'DAV - Reset your Password',
     text: `You are receiving this because you (or someone else) have requested the reset of the password for your DAV community account.
            Please click on the following link, or paste this into your browser to complete the process:
-           https://${req.headers.host}/api/reset/${token}.
+           https://${req.headers.host}/?=${token}#verify
 
            If you did not request this, please ignore this email and your password will remain unchanged.`
   };
